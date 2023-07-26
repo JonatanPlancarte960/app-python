8299fed6cab565f72d7a70ee1bcf46975e37e7bf
JonatanPlancarte960is <jonatanplancarte960is@gmail.com>
2023-07-26 14:28:28 -0600

    New WebApp for Azure

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '¡Hola desde mi Web App de Python 3.11 en Azure App Service!'
+    return'''<h1>¡Hola desde mi Web App de Python 3.11 en Azure App Service!</h1>
+    <br><h2>Youtube: OX I Jonatan</h2>
+    <br><img src="/static/maxresdefault (3).jpg" width="50%" height="50%" alt="Thumbnail 1">
+    <br>
+    <br><img src="/static/maxresdefault (4).jpg" width="50%" height="50%" alt="Thumbnail 2">
+    <br><h2>Github: JonatanPlancarte960</h2>
+    '''
 
 if __name__ == '__main__':
     app.run()
